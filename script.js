ce2cf84e067039cbd9e4c796fcb3349651ecf2a7
Max Huttunen <max.huttunen@gmail.com>
2014-01-31 15:43:56 +0200

    rewrote in pure JS (no more jQuery)

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-$(document).ready(function() {
+try {
 	var pathName = location.pathname.replace(/\//g, "");
 	var magnetLogoURL = chrome.extension.getURL("icon16.png");
-    if(pathName.length === 40) {
-        var torrentName = $(".download>h2>span").html().replace(/<[^>]*>/g, "");
-        var magnetLink = "magnet:?xt=urn:btih:" + pathName + "&dn=" + encodeURI(torrentName);
-        $(".download").children("dl").eq(0).after('<dl><dt><a href="' + magnetLink + '"><span class="u" style="background: transparent url(\'' + magnetLogoURL + '\') no-repeat 5px center;">Magnet</span></a></dt></dl>');
-    } else if(pathName.match(/search/i)) {
-        $(".results>dl").each(function() {
-            var link = $(this).find("dt>a");
-            var torrentName = link.html().replace(/<[^>]*>/g, "");
-            var infoHash = link.attr("href").replace(/\//g, "");
-            var magnetLink = "magnet:?xt=urn:btih:" + infoHash + "&dn=" + encodeURI(torrentName);
-            $(this).find("dt").append('<a href="' + magnetLink + '"><img style="float:right;" src="' + magnetLogoURL + '" /></a>');
-        });
-    }
-});
+	if(pathName.length === 40) {
+		var torrentName = document.querySelector(".download>h2>span").innerHTML.replace(/<[^>]*>/g, "");
+		var magnetLink = "magnet:?xt=urn:btih:" + pathName + "&dn=" + encodeURI(torrentName);
+		document.querySelector(".download dl").insertAdjacentHTML("afterend", '<dl><dt><a href="' + magnetLink + '"><span class="u" style="background: transparent url(\'' + magnetLogoURL + '\') no-repeat 5px center;">Magnet</span></a></dt></dl>');
+	} else if(pathName.match(/search/i)) {
+		var searchItems = document.querySelectorAll(".results>dl");
+		for(var i = 0; i < searchItems.length; i++) {
+			var currentItem = searchItems[i];
+			var link = currentItem.querySelector("dt>a");
+			var torrentName = link.innerHTML.replace(/<[^>]*>/g, "");
+			var infoHash = link.getAttribute("href").replace(/\//g, "");
+			var magnetLinkURI = "magnet:?xt=urn:btih:" + infoHash + "&dn=" + encodeURI(torrentName);
+			currentItem.querySelector("dt").innerHTML += '<a href="' + magnetLink + '"><img style="float:right;" src="' + magnetLogoURL + '" /></a>';
+		}
+	}
+} catch(e) {}
